abeea5538fe99f7184bfa0069c500a125e5f129a
Devmithra Panwar Arya <dev87111@gmail.com>
2024-07-16 16:18:21 +0530

    third commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import cors from "cors";
 
 env.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
