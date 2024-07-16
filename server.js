987e8c9f2b5a2b9ef021e44b5c58f56f0191e6e2
Devmithra Panwar Arya <dev87111@gmail.com>
2024-07-16 17:20:28 +0530

    updated cors link

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ db.connect(function(err) {
   });
 
 app.use(function (req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', 'https://ask-project-frontend.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://askproject.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
