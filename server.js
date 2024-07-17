3d0619e289ffa5b8cb05995920cbb910ba080540
Devmithra Panwar Arya <dev87111@gmail.com>
2024-07-17 20:12:35 +0530

    testing again...

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ db.connect(function(err) {
   });
 
 app.use(function (req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'https://askproject.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
